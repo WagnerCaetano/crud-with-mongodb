9ef8cb2c2f974a4279a23f2b1b3f1bce581e0494
wagnercaetano <wagnercaetano01@gmail.com>
2022-11-27 21:45:53 -0300

    fixing cors

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const ParticipantsService = require("./services/ParticipantsService");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: './connectionString.env' });
 }
